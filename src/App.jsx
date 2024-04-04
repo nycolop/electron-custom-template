2fec0af452f3cbc0eda5ccc72a45cd8831b1f2f1
nycolop <madrazonecogamy@gmail.com>
2024-04-04 14:46:09 -0300

    additions

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="basis-[20%] bg-slate-900 border-r-white border-r-2">
+
+      </div>
+
+      <div className="basis-[80%] bg-slate-500">
+
+      </div>
+    </div>
+  );
+}
